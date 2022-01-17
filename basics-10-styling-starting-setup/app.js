5a231a87217a28b1978fe3b8ed085b24a0d774ef
yirer <xeroonelove@gmail.com>
2022-01-17 17:45:06 +0900

    add: 동적 스타일, 클래스 적용

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+  },
+  methods: {
+    handleSelectBox(box) {
+      console.log(box);
+      switch (box) {
+        case "A":
+          this.boxASelected = !this.boxASelected;
+          break;
+        case "B":
+          this.boxBSelected = !this.boxBSelected;
+          break;
+        case "C":
+          this.boxCSelected = !this.boxCSelected;
+          break;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
