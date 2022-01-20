053b268e9a0b142d5dffaef77e62423f31c8b2d8
yirer <xeroonelove@gmail.com>
2022-01-20 16:00:10 +0900

    add: 기본 컴포넌트 동작 연습 2

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <TheHeader />
+    <!-- <BadgeList />
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+    <course-goals #default="{ item }">
+      <h2>{{ item }}</h2>
+    </course-goals> -->
+    <button @click="setSeletedcomponents('active-goals')">Active Goals</button>
+    <button @click="setSeletedcomponents('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CouresGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSeletedcomponents(comp) {
+      this.selectedComponent = comp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
